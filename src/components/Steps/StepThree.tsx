14a65080d4bec3c92a189b258ae06aa374b012c8
João Vítor Ferraz <git@p1pa.dev>
2024-08-17 23:46:05 -0300

    Width texto card + height nav mobile

--- src/components/Steps/StepThree.tsx
+++ src/components/Steps/StepThree.tsx
@@ -7,18 +7,18 @@ export default function StepOne(props: Props) {
     <StepCard>
       <StepsNav {...props} id={3} />
       <div className="w-full flex-1 flex flex-col justify-center items-center">
-        <p className="text-center py-4 text-base md:text-xl font-regular">
+        <p className="text-justify py-4 text-base md:text-xl font-regular">
           Após a aprovação do orçamento,{' '}
           <span className="font-semibold">
             você pode ter total confiança em minha experiência e
             comprometimento.
           </span>
         </p>
-        <p className="text-center py-4 text-base md:text-xl font-regular">
+        <p className="text-justify py-4 text-base md:text-xl font-regular">
           {' '}
           Estou pronto para criar a estratégia mais eficaz para resolver sua
           situação, garantindo que você tenha o{' '}
-          <span className="block bg-success font-semibold px-2">
+          <span className="bg-success font-semibold px-2">
             melhor suporte jurídico possível.
           </span>
         </p>
