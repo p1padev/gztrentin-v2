dbd517338b7ce59b5d377eb817d63f25adc2ea9a
João Vítor Ferraz <git@p1pa.dev>
2024-08-18 19:06:56 -0300

    Fix settings

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Hello world!
+Site para o Gustavo!
--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
+import js from '@eslint/js';
 import eslintPluginAstro from 'eslint-plugin-astro';
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
+  ...js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   ...eslintPluginAstro.configs['jsx-a11y-recommended'],
   {
